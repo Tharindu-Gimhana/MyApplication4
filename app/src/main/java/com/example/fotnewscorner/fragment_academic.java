726f54dbaa84fa2930889be1b88dc9335ce105cf
Tharindu-Gimhana <167947975+Tharindu-Gimhana@users.noreply.github.com>
2025-06-14 02:50:22 +0530

    add the neccesary layout and javafiles

---
+++ app/src/main/java/com/example/fotnewscorner/fragment_academic.java
@@ -0,0 +1,24 @@
+package com.example.fotnewscorner;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class fragment_academic extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_fragment_academic);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+}
