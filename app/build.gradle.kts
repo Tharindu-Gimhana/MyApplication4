bb80852e494b924d4dcfcc16eb95f878dcfa29b0
Tharindu-Gimhana <167947975+Tharindu-Gimhana@users.noreply.github.com>
2025-06-13 19:31:45 +0530

    add changes to neccesary files

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,13 +1,14 @@
 plugins {
     alias(libs.plugins.android.application)
+    id("com.google.gms.google-services") // Add this for Firebase
 }
 
 android {
-    namespace = "com.example.myapplication"
+    namespace = "com.example.fotnewscorner"
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.example.myapplication"
+        applicationId = "com.example.fotnewscorner"
         minSdk = 24
         targetSdk = 35
         versionCode = 1
@@ -37,7 +38,30 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation(libs.firebase.auth)
+    implementation(libs.credentials)
+    implementation(libs.credentials.play.services.auth)
+    implementation(libs.googleid)
+    implementation(libs.recyclerview)
+    implementation(libs.firebase.database)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+
+    // Import the Firebase BoM (Bill of Materials)
+    implementation(platform(libs.firebase.bom))
+
+// Firebase Authentication
+    implementation(libs.firebase.auth.ktx)
+
+// Cloud Firestore
+    implementation(libs.firebase.firestore.ktx)
+
+    // Room components
+    implementation(libs.room.runtime)
+    annotationProcessor(libs.room.compiler) // Use 'annotationProcessor' for Java projects
+// Kotlin Extensions and Coroutines support for Room (only needed if using Kotlin extensions in Java, otherwise can be omitted)
+    implementation(libs.room.ktx)
+
+
 }
